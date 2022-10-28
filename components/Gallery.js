9ab8b9d4d13e79aa2ab8d228add1a9f257c42282
Johnny C <cheungjc@outlook.com>
2022-10-27 22:54:06 -0400

    infinite scroll functionality

--- components/Gallery.js
+++ components/Gallery.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Gallery.module.css';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
 
 export default function Gallery() {
@@ -9,29 +10,41 @@ export default function Gallery() {
     const [photos, setPhotos] = useState([])
 
     useEffect(() => {
-      const url = "https://api.unsplash.com"
-      const key = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
-  
-      axios.get(`${url}/photos/random?client_id=${key}&count=5`)
-      .then(res => setPhotos([...photos, ...res.data]))
+        getPhotos()
     },[])
+
+    // infinite scroll functionality
+    const getPhotos = () => {
+        const url = "https://api.unsplash.com"
+        const key = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
+    
+        axios.get(`${url}/photos/random?client_id=${key}&count=30`)
+        .then(res => setPhotos([...photos, ...res.data]))
+    }
   
     return (  
         <div className={styles.gallery}>
-            <ResponsiveMasonry
-            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+            <InfiniteScroll
+                dataLength={photos.length}
+                next={getPhotos}
+                hasMore={true}
+                loader={<h3>rendering....</h3>}
             >
-            <Masonry gutter="30px">
-                {photos.map((photo) => (
-                    <img
-                        className={styles.image}
-                        src={photo.urls.full}
-                        key={photo.id}
-                        alt="photo goes here"
-                    />
-                ))}
-            </Masonry>
-        </ResponsiveMasonry>
+                <ResponsiveMasonry
+                columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                >
+                    <Masonry gutter="30px">
+                        {photos.map((photo) => (
+                            <img
+                                className={styles.image}
+                                src={photo.urls.regular}
+                                key={photo.id}
+                                alt="photo goes here"
+                            />
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            </InfiniteScroll>
     </div>
     )
 }
