637faf478f43ee25d9373320411ad1e903f01084
Johnny C <cheungjc@outlook.com>
2022-10-28 18:04:21 -0400

    update styling

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,8 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
     return (
     <div className={styles.header}>
-        <h2>Unsplash Photo Gallery</h2>    
+        <h2>Unsplash Photo Gallery</h2> 
+        <h4>Explore thousands of photos and get inspired!</h4>   
     </div>
     )
 }
